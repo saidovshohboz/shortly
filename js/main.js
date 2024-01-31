96a5ecb1b120a331d1d3fadf07b3075b089e4657
saidovshohboz <shohbozsaidovv@gmail.com>
2024-01-31 20:02:55 +0500

    Add url copy button click handler. Wrap form submit handler  and if

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
+let elUrlShortener = document.querySelector('.url-shortener');
 let elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
 let elUrlShortenerResults = document.querySelector('.url-shortener__results');
 
-elUrlShortenerForm.addEventListener('submit', function (evt) {
-  evt.preventDefault();
+if(elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
 
-  elUrlShortenerResults.classList.add('url-shortener__results--open')
-});
+    elUrlShortenerResults.classList.add('url-shortener__results--open')
+  });
+}
+
+if(elUrlShortener) {
+  elUrlShortener.addEventListener('click', function (evt) {
+    if(evt.target.matches('.js-copy-short-link-button')) {
+      // Change button text
+      evt.target.textContent = 'Copied!';
+
+      // Change button bg-color
+      evt.target.classList.add('url-shortener__copy-button--copied')
+
+      // Reset button text and class after 1 second
+      setTimeout(function () {
+        evt.target.textContent = 'Copy';
+        evt.target.classList.remove('url-shortener__copy-button--copied');
+      }, 1000);
+    }
+  });
+}
